90b23e753fd4bc889b4b49414796fd48d5a0d87d
Felix <fangli8080@gmail.com>
2021-04-02 15:34:57 +0800

    feat: add css

--- src/components/ProcessEditor/index.vue
+++ src/components/ProcessEditor/index.vue
@@ -7,6 +7,11 @@
 import BpmnModeler from 'bpmn-js/lib/Modeler'
 import diagramXML from './resources/newDiagram.bpmn'
 
+import 'bpmn-js/dist/assets/diagram-js.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+
 export default {
   name: 'ProcessEditor',
   data () {
